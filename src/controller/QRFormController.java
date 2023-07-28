c6d53ffbaf0c476e975f4824df9f217e240e4f83
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2023-07-28 12:31:30 +0530

    create qr generator view and controller

---
+++ src/controller/QRFormController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class QRFormController {
+    @FXML
+    private ImageView pic;
+
+    @FXML
+    private TextField txtText;
+
+    @FXML
+    void txtTextOnAction(ActionEvent event) {
+
+    }
+}
